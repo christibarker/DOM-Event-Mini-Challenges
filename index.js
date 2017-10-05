36c8bb970e4153c12dc744fcf4d54fcfafb4352d
Christi Ovchinnikov Barker <kaco7777@gmail.com>
2017-10-05 16:32:48 -0600

    questionthree

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-var changeKey = []
+var changeKey =
 
 document.addEventListener('keyup', function(e){
-	changeKey.push(e.key);
-	document.getElementById('letter').innerHTML = changeKey.join('');
+	changeKey = String(e.key);
+	document.getElementById('letter').innerHTML = changeKey;
 });
 
 
